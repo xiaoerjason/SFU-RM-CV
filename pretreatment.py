62da1cfb005e711c7ee8a70332533efaa029b0c2
xiaoerjiasen <abc77567291@qq.com>
2021-11-09 12:15:59 +0800

    1

---
+++ pretreatment.py
@@ -0,0 +1,54 @@
+import os   
+import pypinyin
+from pypinyin import pinyin, lazy_pinyin
+# 这是修改DJI Robomaster 数据集文件名的脚本，替换中文字符和空格
+# 适配模型：yolov5
+
+# 批量替换文件名的中文字符
+def rename():
+    # 需要替换的文件路径
+    # python路径改变也要重新改写路径
+    user_path="E://DJI ROCO//robomaster_South"
+    # user_path1="E://DJI ROCO//robomaster_Central"
+    # user_path2="E://DJI ROCO//robomaster_Final"
+    # user_path3="E://DJI ROCO//robomaster_North"
+    # user_path4="E://DJI ROCO//robomaster_South"
+
+    image_path=user_path+"//image"
+    image_annotation_path=user_path+"//image_annotation"
+
+    path_list=[image_path,image_annotation_path]
+
+    for i in path_list:
+
+        filelist=os.listdir(i)#该文件夹下所有的文件（包括文件夹）
+
+        for files in filelist:#遍历所有文件
+        
+            Olddir=os.path.join(i,files);#原来的文件路径
+        
+            if os.path.isdir(Olddir):#如果是文件夹则跳过
+        
+                continue
+        
+            filename=os.path.splitext(files)[0]; #获取文件名
+            #把文件名里的汉字转换成其首字母，切割了字符串
+            filename1 = pinyin(filename, style=pypinyin.FIRST_LETTER) 
+            #创建一个空列表
+            filename2 = []
+            for ch in filename1:
+                filename2.extend(ch)
+            #把列表转换成没有间隔的字符串，因为文件名要以字符串形式存在
+            filenameToStr = ''.join(filename2)
+            #空格替换
+            unfilename=filenameToStr.replace(' ','_')
+
+            filetype=os.path.splitext(files)[1];#文件扩展名
+
+            Newdir=os.path.join(i,unfilename + filetype);#新的文件名
+
+            os.rename(Olddir,Newdir);#重命名
+    
+        print(i+" Execution complete")
+    
+rename()
---
+++ xml_to_txt.py
@@ -0,0 +1,60 @@
+
+import xml.etree.ElementTree as ET
+ 
+import pickle
+import os
+from os import listdir , getcwd
+from os.path import join
+import glob
+ 
+classes = ["car","red","blue"]
+# armor_colors=[]
+ 
+def convert(size, box):
+ 
+    dw = 1.0/size[0]
+    dh = 1.0/size[1]
+    x = (box[0]+box[1])/2.0
+    y = (box[2]+box[3])/2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+ 
+def convert_annotation(image_name):
+    #
+    in_file = open('./images_annotation/'+image_name[:-3]+'xml') #xml文件路径
+    #
+    out_file = open('./new_labels/'+image_name[:-3]+'txt', 'w') #转换后的txt文件存放路径
+    f = open('./images_annotation/'+image_name[:-3]+'xml')
+    xml_text = f.read()
+    root = ET.fromstring(xml_text)
+    f.close()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+ 
+    for obj in root.iter('object'):
+        cls = obj.find('name').text
+        if cls=="armor" :
+            cls=obj.find('armor_color').text
+        if (cls not in classes):
+            print(cls)
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
+             float(xmlbox.find('ymax').text))
+        bb = convert((w,h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+wd = getcwd()
+ 
+# if __name__ == '__main__':
+ 
+for image_path in glob.glob("./images/*.jpg"): #每一张图片都对应一个xml文件这里写xml对应的图片的路径
+    image_name = image_path.split('\\')[-1] #
+    convert_annotation(image_name)
